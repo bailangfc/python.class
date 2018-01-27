7a29d4fd70bff842d8eb936123703ad7f22f730d
白郎 <bailangfc@outlook.com>
2018-01-27 22:49:00 +0800

    单继承
    python中类的单继承

---
+++ inherit1.py
@@ -0,0 +1,22 @@
+#python中的继承
+
+#coding: utf-8
+
+ class Person(object):
+     def __init__(self,web_site):
+         self.web=web_site
+
+     def update_web(self,site):
+         self.web=site
+         return self.web
+
+ class LaoQi(Person):
+     def about_me(self,name,site):
+         my_web=self.update_web(site)
+         return {"name":name,"web":my_web}
+
+ if __name__=="__main__":
+     #my=Person("www.itdiffer.com")
+     my=LaoQi("www.itdiffer.com")
+     print my.about_me("bailang","bailang.github.io")
+     print my.web
